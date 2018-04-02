5e3fec4eae7caa8d62e41e239258d0c142f845fa
Gemfield <gemfield@civilnet.cn>
2018-04-02 11:25:41 +0800

    Add function to expel robber and to detect the double feed indicator

--- raise_chickens.py
+++ raise_chickens.py
@@ -1,5 +1,5 @@
 import os
-import glob
+import sys
 import shutil
 import argparse
 import cv2
@@ -7,10 +7,24 @@ import time
 import random
 import numpy as np
 import time
+import tempfile
 
 templates_dir = 'templates'
-screenshot_img = 'current_gemfield_farm.png'
+screenshot_img = '{}/current_gemfield_farm.png'.format(tempfile.gettempdir())
 adb_screenshot_cmd = 'adb exec-out screencap -p > {}'.format(screenshot_img)
+adb_lighter_cmd = 'adb shell input keyevent 221'
+adb_darker_cmd = 'adb shell input keyevent 220'
+adb_screen_stayon_cmd = 'adb shell svc power stayon usb'
+
+def errorMsg(str):
+    print('Error: {}'.format(str) )
+    os.system('wall Error: {}'.format(str))
+    sys.exit(1)
+
+def warningMsg(str):
+    print('Warning: {}'.format(str) )
+    os.system('wall Warning: {}'.format(str))
+    
 
 def swipe(start_x, start_y, end_x, end_y, duration):
     adb_swipe_cmd = 'adb shell input swipe {} {} {} {} {}'.format(start_x, start_y, end_x, end_y, duration)
@@ -19,6 +33,12 @@ def swipe(start_x, start_y, end_x, end_y, duration):
 def getRandomSleep():
     #30 ~ 120 s
     return random.randint(30,90)
+
+def loadTemplate(template_img):
+    rc = cv2.imread(template_img, 0)
+    if rc is None:
+        errorMsg('Template {} not exist.'.format(template_img))
+    return rc
 
 class AntFarm(object):
     def __init__(self, logdir=None):
@@ -31,19 +51,30 @@ class AntFarm(object):
         self.crib_pos = None
 
         self.initTemplate()
+        self.setScreenStayon()
 
         if self.logdir and not os.path.isdir(self.logdir):
             os.makedirs(self.logdir)
 
     def initTemplate(self):
-        self.crib_template = cv2.imread(os.path.join(self.templates_dir, 'crib.png'), 0)
-        self.indicator_template = cv2.imread(os.path.join(self.templates_dir, 'indicator.png'), 0)
-        self.thief_template = cv2.imread(os.path.join(self.templates_dir, 'thief.png'), 0)
+        self.crib_template = loadTemplate(os.path.join(self.templates_dir, 'crib.png') )
+        self.indicator_template = loadTemplate(os.path.join(self.templates_dir, 'indicator.png') )
+        self.double_indicator_template = loadTemplate(os.path.join(self.templates_dir, 'double_indicator.png') )
+        self.thief_template = loadTemplate(os.path.join(self.templates_dir, 'thief.png') )
+        self.robber_template = loadTemplate(os.path.join(self.templates_dir, 'robber.png') )
 
     def scanFarm(self):
-        os.system(adb_screenshot_cmd)
+        adb_rc = 1
+        try:
+            adb_rc = os.system(adb_screenshot_cmd)
+        except Exception as e:
+            errorMsg('Check the phone connection with computer. Detail: {}'.str(e))
+
+        if adb_rc != 0:
+            errorMsg('Check the phone connection with computer.')
+
         if not os.path.isfile(screenshot_img):
-            raise Exception('Could not take a screenshot from adb command. Check your phone connection with computer.')
+            errorMsg('Could not take a screenshot from adb command. Check your phone connection with computer.')
 
         if self.logdir:
             shutil.copyfile(screenshot_img, os.path.join(self.logdir, 'gemfield_farm_{:06d}.png'.format(self.step)))
@@ -52,35 +83,51 @@ class AntFarm(object):
         print('<========================== scan farm {} times ==========================>'.format(self.step))
         self.monitor = cv2.imread(screenshot_img)
         self.resolution = self.monitor.shape[:2]
+        #delete the image
+        os.remove(screenshot_img)
+
+    def setScreenStayon(self):
+        print('preparing to keep screen on...')
+        # os.system(adb_lighter_cmd)
+        # os.system(adb_darker_cmd)
+        os.system(adb_screen_stayon_cmd)
 
     def getCribPos(self):
         rc = self.match(self.crib_template, 0.8)
         return rc
 
     def getIndicatorPos(self):
-        return self.match(self.indicator_template, 0.8)
+        rc = self.match(self.indicator_template, 0.8)
+        if rc is None:
+            rc = self.match(self.double_indicator_template, 0.8)
+
+        return rc
 
     def getThiefPos(self):
         return self.match(self.thief_template, 0.8)
+
+    def getRobberPos(self):
+        return self.match(self.robber_template, 0.8)
         
-    def feed(self, force=False):
+    def feed(self):
         rc = self.getIndicatorPos()
         #still has food left
         if rc:
             print('Still has food left...')
-            if not force:
-                return
-            print('But will force feed to wake up phone...')
+            return
+ 
         if self.crib_pos is None:
             self.crib_pos = self.getCribPos()
 
+        if self.crib_pos is None:
+            errorMsg('Make sure your screen of phone is on...')
+
         x,y = self.crib_pos
         adb_tap_cmd = 'adb shell input tap {} {}'.format(x,y)
-        print('preparing feed gemfield chicken...')
+        print('preparing to feed gemfield chicken with {}...'.format(adb_tap_cmd))
         os.system(adb_tap_cmd)
 
     def expelThief(self):
-        #default left thief
         for i in range(3):
             rc = self.getThiefPos()
             if rc is None:
@@ -89,7 +136,20 @@ class AntFarm(object):
             print('Found thief {}'.format(i))
             x,y = rc
             adb_tap_cmd = 'adb shell input tap {} {}'.format(x,y)
+            warningMsg('preparing to expel the thief with {}...'.format(adb_tap_cmd))
             os.system(adb_tap_cmd)
+            self.scanFarm()
+
+    def expelRobber(self):
+        rc = self.getRobberPos()
+        if rc is None:
+            print('No robber found...')
+            return
+        print('Found robber...')
+        x,y = rc
+        adb_tap_cmd = 'adb shell input tap {} {}'.format(x,y)
+        warningMsg('preparing to expel the robber with {}...'.format(adb_tap_cmd))
+        os.system(adb_tap_cmd)
 
 
     def match(self, template, threshold):
@@ -112,7 +172,8 @@ class AntFarm(object):
     def play(self):
         self.scanFarm()
         self.expelThief()
-        self.feed(self.step % 15 == 2)
+        self.expelRobber()
+        self.feed()
         time.sleep(getRandomSleep())
 
 if __name__ == "__main__":
